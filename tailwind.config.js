f5bc80b51de5413b3f61c055a1dd8768d633a96d
rajsanjeev9113 <rajsanjeev9113@gmail.com>
2025-04-13 18:10:09 +0000

    configured tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./login.html", "./signup.html", "./test.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
