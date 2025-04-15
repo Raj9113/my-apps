06efabd7281567659ec54afaa99665d1d0409b68
Raj9113 <94070105+Raj9113@users.noreply.github.com>
2025-04-15 20:14:19 +0000

    Redesign site

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./login.html", "./signup.html", "./test.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
